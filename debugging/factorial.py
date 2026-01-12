cf3b71ae8c99d5668abfeec166c3809f4073a297
zap9412 <thelyaan444@hotmail.com>
2026-01-12 12:26:20 +0100

    Python Debugging

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,20 @@
 import sys
 
 def factorial(n):
+    if n < 0:
+        raise ValueError("Le factoriel n'est pas défini pour les nombres négatifs")
+
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # ← correction essentielle
+        n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) != 2:
+    print("Usage: python3 factorial.py <nombre>")
+else:
+    try:
+        n = int(sys.argv[1])
+        print(factorial(n))
+    except ValueError as e:
+        print("Erreur :", e)
